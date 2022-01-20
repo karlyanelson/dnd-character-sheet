13a6851906160b03ae2754f1fa124729c1150a70
Nelson, Karly <karly.nelson@hey.com>
2022-01-19 21:30:27 -0600

    fix AccessibleInput typing issue; add keyboard avoiding view; testing with lots of input fields

--- src/screens/CharacterSheet/Abilities/Details.js
+++ src/screens/CharacterSheet/Abilities/Details.js
@@ -1,45 +1,138 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, StyleSheet, SafeAreaView } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  KeyboardAvoidingView,
+  ScrollView,
+  SafeAreaView,
+  Platform,
+} from "react-native";
+
+import { HeaderHeightContext } from "@react-navigation/elements";
+
+const AccessibleInput = ({ label, value, onChangeText, ...props }) => {
+  const [borderColor, setBorderColor] = useState("gray");
+
+  const styles = StyleSheet.create({
+    container: {
+      marginVertical: 8,
+    },
+    input: {
+      borderWidth: 2,
+      padding: 16,
+      borderRadius: 8,
+      borderColor,
+      marginBottom: 16,
+    },
+  });
+
+  return (
+    <View accessible={true} accessibilityLabel={label} style={styles.container}>
+      <Text>{label}</Text>
+      <TextInput
+        value={value}
+        style={styles.input}
+        onChangeText={onChangeText}
+        onFocus={() => {
+          setBorderColor("blue");
+        }}
+        onBlur={() => {
+          setBorderColor("gray");
+        }}
+        {...props}
+      />
+    </View>
+  );
+};
 
 const Details = ({ navigation, route }) => {
   const ability = route.params;
 
   const [key, setKey] = useState(ability.key);
   const [title, setTitle] = useState(ability.title);
-
-  const AccessibleInput = ({ label, value, onChangeText }) => {
-    return (
-      <View accessible={true} accessibilityLabel={label}>
-        <Text>{label}</Text>
-        <TextInput
-          value={value}
-          style={styles.input}
-          onChangeText={onChangeText}
-        />
-      </View>
-    );
-  };
+  const [modifier, setModifier] = useState(`${ability.modifier}`);
+  const [total, setTotal] = useState(`${ability.total}`);
 
   return (
-    <SafeAreaView>
-      <AccessibleInput label="Key" value={key} onChangeText={setKey} />
-
-      <View accessible={true} accessibilityLabel="Title">
-        <Text>Title</Text>
-        <TextInput value={title} style={styles.input} onChangeText={setTitle} />
-      </View>
-
-      {/* <Text>{ability.modifier}</Text>
-      <Text>{ability.total}</Text> */}
-    </SafeAreaView>
+    <HeaderHeightContext.Consumer>
+      {(headerHeight) => (
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          contentContainerStyle={{ flex: 1 }}
+          keyboardVerticalOffset={headerHeight}
+        >
+          <ScrollView>
+            <View style={{ padding: 24, paddingBottom: 48 }}>
+              <AccessibleInput label="Key" value={key} onChangeText={setKey} />
+              <AccessibleInput
+                label="Title"
+                value={title}
+                onChangeText={setTitle}
+              />
+              <AccessibleInput
+                label="Title"
+                value={title}
+                onChangeText={setTitle}
+              />
+              <AccessibleInput
+                label="Title"
+                value={title}
+                onChangeText={setTitle}
+              />
+              <AccessibleInput
+                label="Title"
+                value={title}
+                onChangeText={setTitle}
+              />
+              <AccessibleInput
+                label="Title"
+                value={title}
+                onChangeText={setTitle}
+              />
+              <AccessibleInput
+                label="Title"
+                value={title}
+                onChangeText={setTitle}
+              />
+              <AccessibleInput
+                label="Title"
+                value={title}
+                onChangeText={setTitle}
+              />
+              <AccessibleInput
+                label="Title"
+                value={title}
+                onChangeText={setTitle}
+              />
+              <AccessibleInput
+                label="Title"
+                value={title}
+                onChangeText={setTitle}
+              />
+              <AccessibleInput
+                label="Title"
+                value={title}
+                onChangeText={setTitle}
+              />
+              <AccessibleInput
+                label="Modifier"
+                value={modifier}
+                onChangeText={setModifier}
+              />
+              <AccessibleInput
+                label="Total"
+                value={total}
+                onChangeText={setTotal}
+                keyboardType="numbers-and-punctuation"
+              />
+            </View>
+          </ScrollView>
+        </KeyboardAvoidingView>
+      )}
+    </HeaderHeightContext.Consumer>
   );
 };
-
-const styles = StyleSheet.create({
-  input: {
-    borderWidth: 1,
-    padding: 16,
-  },
-});
 
 export default Details;
